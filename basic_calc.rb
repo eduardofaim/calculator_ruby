0ca8137d01d0b66147294b16401816f46d2cd1b4
Edu Faim <eduardo.faim@amedigital.com>
2021-12-28 11:38:41 -0300

    uploading subtraction method

--- basic_calc.rb
+++ basic_calc.rb
@@ -1,13 +1,43 @@
 #Calculator in Ruby
 
+puts "Would you like to:
 
-puts "Enter a Number:"
-num1 = gets.chomp()
+ADD ( + ) ========> [ 1 ]
+SUBTRACT ( - ) ===> [ 2 ]
+MULTIPLY ( * ) ===> [ 3 ]
+DIVIDE ( / ) =====> [ 4 ]
+EXIT / QUIT ======> [ E ]"
 
-puts "Enter another Number:"
-num2 = gets.chomp()
+operator = gets.chomp()
 
-result = (num1.to_f + num2.to_f)
+case operator.downcase
 
-puts "The sum is: #{num1} + #{num2} = #{result}"
+when '1'
+    def addition_operation
+    puts "\n Enter a Number:"
+    num1 = gets.chomp()
+    
+    puts "\n Enter another Number:"
+    num2 = gets.chomp()
+
+    result = (num1.to_f + num2.to_f)
+
+    puts "\n The sum is: #{num1} + #{num2} = #{result}"
+    end
+    addition_operation()
+
+when '2'
+    def subtraction_operation
+    puts "\n Enter a Number:"
+    num1 = gets.chomp()
+    
+    puts "\n Enter another Number:"
+    num2 = gets.chomp()
+
+    result = (num1.to_f - num2.to_f)
+
+    puts "\n The subtraction is: #{num1} - #{num2} = #{result}"
+    end
+    subtraction_operation()
+end
 
