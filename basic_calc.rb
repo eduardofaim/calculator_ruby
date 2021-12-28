19c343ce2425d3c82c58bc5df4466291d9149474
Edu Faim <eduardo.faim@amedigital.com>
2021-12-28 11:23:34 -0300

    creating add operator for basic calc

---
+++ basic_calc.rb
@@ -0,0 +1,13 @@
+#Calculator in Ruby
+
+
+puts "Enter a Number:"
+num1 = gets.chomp()
+
+puts "Enter another Number:"
+num2 = gets.chomp()
+
+result = (num1.to_f + num2.to_f)
+
+puts "The sum is: #{num1} + #{num2} = #{result}"
+
